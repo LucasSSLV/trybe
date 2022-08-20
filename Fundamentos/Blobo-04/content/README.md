896f0536aa03f4901346873f5cc3f12886c74af4
LucasSSLV <lukinhas.kell@hotmail.com>
2022-08-20 11:54:04 -0300

    resolvido variaveis e operações aritméticas

---
+++ Fundamentos/Blobo-04/content/README.md
@@ -0,0 +1,1 @@
+aqui pratico o conteudo do course
---
+++ Fundamentos/Blobo-04/content/b4-primeiros-passos.js
@@ -0,0 +1,18 @@
+let myName = "lucas";
+let birthCity = "viçosa-Ce";
+let birthYear = 1995;
+console.log(myName, birthCity, birthYear);
+//
+birthYear = 2030;
+console.log(birthYear);
+//
+birthCity = 'sobral';
+console.log(birthCity);
+//
+const base = 5;
+let heigth = 8;
+const area = base * heigth;
+console.log(area);
+//
+const perimeter = heigth + heigth;
+console.log(perimeter);
