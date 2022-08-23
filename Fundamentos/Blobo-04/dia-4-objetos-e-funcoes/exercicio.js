9376324db0e9422febade9c9051a36f5eb12a341
LucasSSLV <lukinhas.kell@hotmail.com>
2022-08-23 19:18:09 -0300

    questão resolvida

--- Fundamentos/Blobo-04/dia-4-objetos-e-funcoes/exercicio.js
+++ Fundamentos/Blobo-04/dia-4-objetos-e-funcoes/exercicio.js
@@ -1,7 +1,24 @@
 //Q1
-let info = {
-    personagem: 'Margarida',
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+//   console.log('seja bem-vinda, ' + info.personagem)
+
+//Q2
+ let info = {
+     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-  console.log('seja bem-vinda, ' + info.personagem)
+    recorrente: 'Sim'
+}; 
+//    for(let key in info){
+//     console.log(info[key])
+//   }
+
+//Q3
+
+  for(let key in info){
+    console.log(key);
+  }
