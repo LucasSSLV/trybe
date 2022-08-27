5b5a43fef41b56555faf73b6358510a3909005e1
LucasSSLV <lukinhas.kell@hotmail.com>
2022-08-27 16:44:34 -0300

    questão 2 resolvida

--- Fundamentos/Blobo-04/dia-1-primeiros-passos/exercicio-if-&-else.js
+++ Fundamentos/Blobo-04/dia-1-primeiros-passos/exercicio-if-&-else.js
@@ -1,2 +1,14 @@
 //1
 const nota = 99;
+//2
+if (nota >= 80){
+  console.log("Parabéns, você foi aprovada(o)!");  
+}
+
+if (nota >= 60 && nota < 80){
+    console.log("Você está na nossa lista de espera");
+}
+
+if (nota < 60){
+    console.log("Você foi reprovada(o)");
+} 
