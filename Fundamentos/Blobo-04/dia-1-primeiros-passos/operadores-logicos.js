8f3d53aadbc1ecd10162171a434486b35a15558c
LucasSSLV <lukinhas.kell@hotmail.com>
2022-08-28 17:33:44 -0300

    questão 4 -imprimir let=message fora das condições resolvida

--- Fundamentos/Blobo-04/dia-1-primeiros-passos/operadores-logicos.js
+++ Fundamentos/Blobo-04/dia-1-primeiros-passos/operadores-logicos.js
@@ -8,18 +8,37 @@ let message = '';
 
 //3
 
+// if (currenHour >= 22){
+//     console.log("Não deveríamos comer nada, é hora de dormir")
+// }
+// if (currenHour >= 18 && currenHour < 22){
+//     console.log("Rango da noite, vamos jantar :D")
+// }
+// if(currenHour >= 14 && currenHour < 18){
+//     console.log("Vamos fazer um bolo pro café da tarde?")
+// }
+// if(currenHour >= 11 && currenHour < 14){
+//     console.log("Hora do almoço!!!")
+// }
+// if (currenHour >= 4 && currenHour < 11){
+//     console.log("Hmmm, cheiro de café recém passado")
+// }
+
+//4
+
 if (currenHour >= 22){
-    console.log("Não deveríamos comer nada, é hora de dormir")
+    message = "Não deveríamos comer nada, é hora de dormir";
 }
 if (currenHour >= 18 && currenHour < 22){
-    console.log("Rango da noite, vamos jantar :D")
+    message = "Rango da noite, vamos jantar :D";
 }
 if(currenHour >= 14 && currenHour < 18){
-    console.log("Vamos fazer um bolo pro café da tarde?")
+    message = "Vamos fazer um bolo pro café da tarde?";
 }
 if(currenHour >= 11 && currenHour < 14){
-    console.log("Hora do almoço!!!")
+    message = "Hora do almoço!!!";
 }
 if (currenHour >= 4 && currenHour < 11){
-    console.log("Hmmm, cheiro de café recém passado")
+    message = "Hmmm, cheiro de café recém passado";
 }
+console.log(message);
