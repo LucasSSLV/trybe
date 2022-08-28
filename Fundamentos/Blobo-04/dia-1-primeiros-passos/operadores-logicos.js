91b16640f18be1f93f16d9edbc5c88e9e878902b
LucasSSLV <lukinhas.kell@hotmail.com>
2022-08-28 17:24:26 -0300

    questão 3 testada e resolvida

--- Fundamentos/Blobo-04/dia-1-primeiros-passos/operadores-logicos.js
+++ Fundamentos/Blobo-04/dia-1-primeiros-passos/operadores-logicos.js
@@ -5,3 +5,21 @@ const currenHour = 10;
 //2
 
 let message = '';
+
+//3
+
+if (currenHour >= 22){
+    console.log("Não deveríamos comer nada, é hora de dormir")
+}
+if (currenHour >= 18 && currenHour < 22){
+    console.log("Rango da noite, vamos jantar :D")
+}
+if(currenHour >= 14 && currenHour < 18){
+    console.log("Vamos fazer um bolo pro café da tarde?")
+}
+if(currenHour >= 11 && currenHour < 14){
+    console.log("Hora do almoço!!!")
+}
+if (currenHour >= 4 && currenHour < 11){
+    console.log("Hmmm, cheiro de café recém passado")
+}
