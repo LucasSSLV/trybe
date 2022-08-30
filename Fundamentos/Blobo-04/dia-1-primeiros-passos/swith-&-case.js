192e367e96c36118ba7da99ea0ee48dc7f74d5fb
LucasSSLV <lukinhas.kell@hotmail.com>
2022-08-29 21:54:20 -0300

    questão 2 resolvida e testada, questão corrigida e add comentario referente a mesma -//1

--- Fundamentos/Blobo-04/dia-1-primeiros-passos/swith-&-case.js
+++ Fundamentos/Blobo-04/dia-1-primeiros-passos/swith-&-case.js
@@ -1,1 +1,22 @@
-let estado = 'aprovada, lista, reprovada';
+//1
+
+let estado = '';
+
+//2
+
+situacao = 'lista';
+
+switch (situacao) {
+    case 'aprovado':
+        estado = "Parabéns, você foi aprovada(o)!";
+        break; 
+        case 'lista':
+        estado = 'você esta na nossa lista de espera';
+        break;
+        case 'reprovado':
+        estado = 'Você foi reprovado';
+        break;
+        default:
+            console.log('não se aplica');
+}
+    console.log(estado);
