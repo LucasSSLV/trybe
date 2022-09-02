b9e7776a83621127a469ac064769c9fe76eea8d4
LucasSSLV <lukinhas.kell@hotmail.com>
2022-09-02 16:08:27 -0300

    exercicio 1 linha 4 corrigido/ questão 2 resolvida

--- Fundamentos/Blobo-04/dia-2-array-e-loop-for/conteudo.js
+++ Fundamentos/Blobo-04/dia-2-array-e-loop-for/conteudo.js
@@ -1,6 +1,13 @@
 // obtenha o valor 'Serviços' do array menu
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = [1];
+let menuServices = menu[1];
 
 console.log(menuServices);
+
+// Procure o índice do valor "Portfólio" do array menu:
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio);
