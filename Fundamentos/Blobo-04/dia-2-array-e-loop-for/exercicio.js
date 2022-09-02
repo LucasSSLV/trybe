c50385150a4ae35a9de1919f8184d3fc46f397c9
LucasSSLV <lukinhas.kell@hotmail.com>
2022-09-02 18:02:50 -0300

    questão 1 resolvida

---
+++ Fundamentos/Blobo-04/dia-2-array-e-loop-for/exercicio.js
@@ -0,0 +1,7 @@
+// Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1 imprimir todos os valores
+
+    console.log(numbers);
