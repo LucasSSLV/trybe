e650b04f5df1f706df15cc18877bd953d152948c
LucasSSLV <lukinhas.kell@hotmail.com>
2022-09-02 15:17:13 -0300

    questão 5 resolvida

--- Fundamentos/Blobo-04/dia-1-primeiros-passos/exercicio.js
+++ Fundamentos/Blobo-04/dia-1-primeiros-passos/exercicio.js
@@ -1,63 +1,77 @@
-//1 fazer 5 programas 
+// //1 fazer 5 programas 
 
-//adiçao
+// //adiçao
 
-let adi = (a + b);
+// let adi = (a + b);
 
-//subtração
+// //subtração
 
-let sub = (a - b);
+// let sub = (a - b);
 
-//multiplicação
+// //multiplicação
 
-let mult = (a * b);
+// let mult = (a * b);
 
-//divisão
+// //divisão
 
-let div = (a / b);
+// let div = (a / b);
 
-//módulo
+// //módulo
 
-let mod = (a % b);
+// let mod = (a % b);
 
-//2 Retorne o maior de dois numeros
+// //2 Retorne o maior de dois numeros
 
-let num1 = -20;
-let num2 = 2;
+// let num1 = -20;
+// let num2 = 2;
 
-if (num1 > num2){
-   console.log(num1);
-}
+// if (num1 > num2){
+//    console.log(num1);
+// }
 
-if (num2 > num1){
-   console.log(num2);
-}
+// if (num2 > num1){
+//    console.log(num2);
+// }
 
-3//Retorne o maior de 3 numeros
+// 3//Retorne o maior de 3 numeros
 
-function maior(num1, num2, num3){
+// function maior(num1, num2, num3){
 
-let maior = num1;
+// let maior = num1;
    
-if(num2 > maior)
-      maior = num2;
-if(num3 > maior)
-      maior = num3; 
-console.log(maior);
-   }
-maior(1, 23, -3);
+// if(num2 > maior)
+//       maior = num2;
+// if(num3 > maior)
+//       maior = num3; 
+// console.log(maior);
+//    }
+// maior(1, 23, -3);
 
-4//se positivo retorne positivo, se negativo retorne negativo
+// 4//se positivo retorne positivo, se negativo retorne negativo
 
-function positivoNegativo(valor){
-   if(valor > 0){
-      console.log("positivo");
+// function positivoNegativo(valor){
+//    if(valor > 0){
+//       console.log("positivo");
+//    }
+//    else if(valor < 0){
+//       console.log("negativo");
+//    }
+//    else{
+//       console.log("Zero");
+//    }
+// }
+// positivoNegativo(5 - 5);
+
+// 5 somar angulos internos de um triangulo
+
+function angulosInternos(angulos){
+   let values = [];
+   for(let i = 0; i < angulos.length; i += 1){
+      values = angulos[i];
    }
-   else if(valor < 0){
-      console.log("negativo");
+   if(values <= 0 || values > 188){
+      return false;
    }
-   else{
-      console.log("Zero");
+      return true;
    }
-}
-positivoNegativo(5 - 5);
+console.log(angulosInternos([1, 34, -18]))
