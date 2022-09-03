4428df8e5104e4f30822eabad2782acd09c83130
LucasSSLV <lukinhas.kell@hotmail.com>
2022-09-03 17:24:33 -0300

    questão 2 resolvida

--- Fundamentos/Blobo-04/dia-2-array-e-loop-for/exercicio.js
+++ Fundamentos/Blobo-04/dia-2-array-e-loop-for/exercicio.js
@@ -5,3 +5,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //1 imprimir todos os valores
 
     console.log(numbers);
+
+//2 some todos os valores e imprima
+
+let sun = 0; 
+
+for(let i = 0; i < numbers.length; i += 1){
+   
+   sun += numbers[i];
+}
+    console.log(sun);
