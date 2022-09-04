8f1248ea3a1f7deb6b6089884332275f74d7e1d0
LucasSSLV <lukinhas.kell@hotmail.com>
2022-09-04 15:53:26 -0300

    questão 3 resolvida

--- Fundamentos/bloco5-JS-eventos-e-web-storage/dia1-dom-&-seletores/script.js
+++ Fundamentos/bloco5-JS-eventos-e-web-storage/dia1-dom-&-seletores/script.js
@@ -5,3 +5,7 @@ document.getElementById('page-title').innerHTML = 'Bastaros Inglorios';
 //2 Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 
 document.getElementById('second-paragraph').innerText = "folhas ao ver não estão voando e nem livres";
+
+//3 Por fim, recupere o subtítulo e altere-o também.
+
+document.getElementById('subtitle').innerText ='observe à liberdade, não vive-a';
