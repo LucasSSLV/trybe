4d412d97f3ea318b770c1d67c4fb9795f14a69a0
LucasSSLV <lukinhas.kell@hotmail.com>
2022-09-04 15:48:29 -0300

    questão 1 resolvida

---
+++ Fundamentos/bloco5-JS-eventos-e-web-storage/dia1-dom-&-seletores/script.js
@@ -0,0 +1,3 @@
+//1 Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+
+document.getElementById('page-title').innerHTML = 'Bastaros Inglorios';
