7f46c1b0cb02e77b1b1f745ea6dfec9646a6416b
LucasSSLV <lukinhas.kell@hotmail.com>
2022-09-04 15:50:35 -0300

    questão 2 resolvida

--- Fundamentos/bloco5-JS-eventos-e-web-storage/dia1-dom-&-seletores/script.js
+++ Fundamentos/bloco5-JS-eventos-e-web-storage/dia1-dom-&-seletores/script.js
@@ -1,3 +1,7 @@
 //1 Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
 
 document.getElementById('page-title').innerHTML = 'Bastaros Inglorios';
+
+//2 Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+
+document.getElementById('second-paragraph').innerText = "folhas ao ver não estão voando e nem livres";
