908d87aa5588f407b6cd66d4fc4803c37caf3b2a
lucas santos da silva <lukinhas.kell@hotmail.com>
2022-09-10 12:23:21 -0300

    test

---
+++ Fundamentos/bloco5-JS-eventos-e-web-storage/exercio-pt2/script.js
@@ -0,0 +1,15 @@
+// Crie um irmão para elementoOndeVoceEsta.
+
+let irmao = document.createElement('p');
+console.log(irmao)
+
+
+// Crie um filho para elementoOndeVoceEsta.
+
+
+
+// Crie um filho para primeiroFilhoDoFilho.
+
+
+
+// A partir desse filho criado, acesse terceiroFilho.
